e1c375590e5f5c8a7c38f9bbf66a05cbd5d2dbc5
smitkiri <smit.kiri@gmail.com>
2021-11-30 10:55:21 -0500

    Change scheduler implementation

--- scheduler.py
+++ scheduler.py
@@ -2,40 +2,34 @@ import numpy as np
 
 
 class ExponentialSchedule:
-    def __init__(self, value_from: float, value_to: float, num_steps: int):
-        """Exponential schedule from `value_from` to `value_to` in `num_steps` steps.
-
-        $value(t) = a \exp (b t)$
+    def __init__(self, value_from: float, value_to: float, eps_decay: float = None, num_steps: int = None):
+        """
+        Decay from `value_from` to `value_to` with `eps_decay ^ step_number`
 
         :param value_from: initial value
         :param value_to: final value
-        :param num_steps: number of steps for the exponential schedule
+        :param eps_decay: decay rate
+        :param num_steps: The number of steps to get to `value_to`
         """
         self.value_from = value_from
         self.value_to = value_to
         self.num_steps = num_steps
 
-        self.a = value_from
-        self.b = (np.log(value_to) - np.log(self.a)) / (num_steps - 1)
-
-    def value(self, step: int) -> float:
-        """Return exponentially interpolated value between `value_from` and `value_to`interpolated value between.
-
-        returns {
-            `value_from`, if step == 0 or less
-            `value_to`, if step == num_steps - 1 or more
-            the exponential interpolation between `value_from` and `value_to`, if 0 <= steps < num_steps
-        }
-
-        :param step:  The step at which to compute the interpolation.
-        :rtype: float.  The interpolated value.
-        """
-
-        if step <= 0:
-            value = self.value_from
-        elif step >= self.num_steps:
-            value = self.value_to
+        if self.num_steps is not None:
+            self.eps_decay = np.exp(np.log(self.value_to) / self.num_steps)
         else:
-            value = self.a * np.exp(self.b * step)
+            self.eps_decay = eps_decay
+
+    def value(self, episode_num: int) -> float:
+        """
+        Get the epsilon value for the current episode
+
+        :param episode_num: Current episode number
+        :return: Epsilon
+        """
+        value = self.value_from * (self.eps_decay ** episode_num)
+
+        if value < self.value_to:
+            value = self.value_to
 
         return value
